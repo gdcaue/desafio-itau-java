c3a6e89bbdc5ed958ee9ed0b6d19958d49928339
Caue Duarte <caue.next4@gmail.com>
2025-08-17 19:02:45 -0300

    doc: insere comentarios na classe principal

--- src/main/java/br/com/itau/desafio/DesafioApplication.java
+++ src/main/java/br/com/itau/desafio/DesafioApplication.java
@@ -3,11 +3,21 @@ package br.com.itau.desafio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principal da aplicação.
+ *
+ * É o ponto de entrada do Spring Boot, responsável por inicializar
+ * todo o contexto da aplicação e subir o servidor embutido.
+ */
 @SpringBootApplication
 public class DesafioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);
-	}
-
+    /**
+     * Método main responsável por iniciar a aplicação.
+     *
+     * @param args argumentos de linha de comando
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApplication.class, args);
+    }
 }
